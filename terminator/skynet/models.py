6c36493120b4f284e7adacf5f484f59d261278bf
Igor Wang <igor@igorw.org>
2015-11-13 16:31:10 -0500

    Add models, updated database

--- terminator/skynet/models.py
+++ terminator/skynet/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class TopDomain(models.Model):
+    domain_name = models.URLField()
+    accept_count = models.PositiveIntegerField(default=0)
+    reject_count = models.PositiveIntegerField(default=0)
+
+
+class FullRequest(models.Model):
+    page_url = models.URLField()
+    accept_count = models.PositiveIntegerField(default=0)
+    reject_count = models.PositiveIntegerField(default=0)
+    top_domain = models.ForeignKey(TopDomain, default=None)
+
+    @property
+    def __str__(self):
+        return self.page_url
+
+
+class LeakToURL(models.Model):
+    leak_url = models.URLField()
+    leak_type = models.PositiveSmallIntegerField()
+    leak_from = models.ForeignKey(FullRequest)
+
+    @property
+    def __str__(self):
+        return self.leak_url
+
