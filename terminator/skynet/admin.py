9073b0bd2647099da8eb6e63308e655a24496afd
Igor Wang <igor@igorw.org>
2015-11-13 16:41:38 -0500

    Add database forms to admin view

--- terminator/skynet/admin.py
+++ terminator/skynet/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(TopDomain)
+admin.site.register(FullRequest)
+admin.site.register(LeakToURL)
