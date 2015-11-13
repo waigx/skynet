c4300b0d199ad69e6bbe0841903006c95136b145
Igor Wang <igor@igorw.org>
2015-11-13 17:50:26 -0500

    Admin manager page init version finished

--- terminator/skynet/admin.py
+++ terminator/skynet/admin.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(TopDomain)
-admin.site.register(FullRequest)
-admin.site.register(LeakToURL)
+
+class TopDomainAdmin(admin.ModelAdmin):
+    list_display = ['domain_name', 'is_leak', 'accept_count', 'reject_count']
+    fieldsets = [
+        ('Domain Name', {'fields': ['domain_name']}),
+        ('Leaked', {'fields': ['is_leak']}),
+        ('Accepted', {'fields': ['accept_count']}),
+        ('Rejected', {'fields': ['reject_count']}),
+    ]
+
+
+class FullRequestAdmin(admin.ModelAdmin):
+    list_display = ['page_url', 'is_leak', 'accept_count', 'reject_count', 'top_domain_name']
+    fieldsets = [
+        ('Request URL', {'fields': ['page_url']}),
+        ('Leaked', {'fields': ['is_leak']}),
+        ('Accepted', {'fields': ['accept_count']}),
+        ('Rejected', {'fields': ['reject_count']}),
+        ('Top Domain', {'fields': ['top_domain']}),
+    ]
+
+    def top_domain_name(self, obj):
+        return obj.top_domain.domain_name
+
+
+class LeakedToURLAdmin(admin.ModelAdmin):
+    list_display = ['leak_url', 'leak_type', 'leak_from_domain']
+    fieldsets = [
+        ('Leak To', {'fields': ['leak_url']}),
+        ('Leak Type', {'fields': ['leak_type']}),
+        ('Leak From', {'fields': ['leak_from_domain']}),
+    ]
+
+    def leak_from_domain(self, obj):
+        return obj.leak_from.page_url
+
+
+admin.site.register(TopDomain, TopDomainAdmin)
+admin.site.register(FullRequest, FullRequestAdmin)
+admin.site.register(LeakToURL, LeakedToURLAdmin)
--- terminator/skynet/models.py
+++ terminator/skynet/models.py
@@ -3,17 +3,26 @@ from django.db import models
 
 class TopDomain(models.Model):
     domain_name = models.URLField()
+    is_leak = models.BooleanField(default=False)
     accept_count = models.PositiveIntegerField(default=0)
     reject_count = models.PositiveIntegerField(default=0)
 
+    def __unicode__(self):
+        return self.domain_name
+
+    def __str__(self):
+        return self.domain_name
 
 class FullRequest(models.Model):
     page_url = models.URLField()
+    is_leak = models.BooleanField(default=False)
     accept_count = models.PositiveIntegerField(default=0)
     reject_count = models.PositiveIntegerField(default=0)
     top_domain = models.ForeignKey(TopDomain, default=None)
 
-    @property
+    def __unicode__(self):
+        return self.page_url
+
     def __str__(self):
         return self.page_url
 
@@ -22,8 +31,3 @@ class LeakToURL(models.Model):
     leak_url = models.URLField()
     leak_type = models.PositiveSmallIntegerField()
     leak_from = models.ForeignKey(FullRequest)
-
-    @property
-    def __str__(self):
-        return self.leak_url
-
