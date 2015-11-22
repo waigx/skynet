f1552c679ccdb5edcb7f822ad0fa5c78a176746e
Yigong Wang <yigwang@cs.stonybrook.edu>
2015-11-22 14:20:16 -0500

    Add boundary case check in to_top_domain function

--- server/skynet/utils/toTopDomain.py
+++ server/skynet/utils/toTopDomain.py
@@ -1,4 +1,3 @@
-
 def to_top_domain(raw_url):
     if (raw_url.find('http://') == 0) or (raw_url.find('https://') == 0):
         has_header = True
@@ -6,4 +5,6 @@ def to_top_domain(raw_url):
         has_header = False
     domain_url = raw_url.split('/')[2 if has_header else 0]
     domain_url = '.'.join(domain_url.split('.')[-2:])
-    return domain_url
+    domain_url = ''.join(domain_url.split(' '))
+    return domain_url if domain_url != '' else 'Unknown'
+
